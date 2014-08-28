e512f34fc5187c9065dac2ac048548cc7bda3c4a
Igor Matyushkin <igormatyushkin014@gmail.com>
2014-08-29 01:26:50 +0400

    Changed PSGestures header file

--- PSGestures.h
+++ PSGestures.h
@@ -1,17 +1,11 @@
 //
 //  PSGestures.h
-//  TestApp
 //
 //  Created by Igor Matyushkin on 28.08.14.
 //  Copyright (c) 2014 Igor Matyushkin. All rights reserved.
 //
 
-#ifndef TestApp_PSGestures_h
-#define TestApp_PSGestures_h
-
 #import "PSTransformableView.h"
 #import "PSTransformableViewDelegate.h"
 #import "PSTransformableViewGestureType.h"
 #import "PSTransformableViewGestureState.h"
-
-#endif
